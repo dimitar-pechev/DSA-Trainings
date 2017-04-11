3d89b947390ba20d14f06d4a88f2b2c0204bd2ac
dimitar-pechev <dimpechev@gmail.com>
2017-04-11 17:29:17 +0300

    more combinatorics tasks

---
+++ Combinatorics/SubesequencesSum/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SubesequencesSum
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var n = input[0];
+                var k = input[1];
+
+                var sequenceSum = Console.ReadLine().Split(' ').Select(int.Parse).ToArray().Sum();
+
+                var subsequenceSum = CalculateBinom(n - 1, k) * sequenceSum;
+
+                Console.WriteLine(subsequenceSum);
+            }
+        }
+
+        public static long CalculateBinom(int n, int k)
+        {
+            long result = 1;
+            for (int i = n; i >= (n - k + 1); i--)
+            {
+                result *= i;
+            }
+
+            long denominator = 1;
+            for (int i = k; i >= 1; i--)
+            {
+                denominator *= i;
+            }
+
+            result /= denominator;
+
+            return result;
+        }
+    }
+}
--- Combinatorics/ZigZagLines/Startup.cs
+++ Combinatorics/ZigZagLines/Startup.cs
@@ -1,9 +1,74 @@
-﻿namespace ZigZagLines
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZigZagLines
 {
     public class Startup
     {
+        private static int counter;
+
         public static void Main()
         {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = input[0];
+            var k = input[1];
+
+            var variations = new List<int[]>();
+            GetVariations(n, k, new int[k], new bool[n]);
+
+            Console.WriteLine(counter);
+        }
+
+        public static void GetVariations(int n, int k, int[] currentVariation, bool[] canPlaceNumber, int index = 0)
+        {
+            if (index == k)
+            {
+                counter++;
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!canPlaceNumber[i])
+                {
+                    var previous = index != 0 ? currentVariation[index - 1] : 0;
+
+                    if (!IsValidNumber(index, i, previous))
+                    {
+                        continue;
+                    }
+
+                    canPlaceNumber[i] = true;
+                    currentVariation[index] = i;
+                    GetVariations(n, k, currentVariation, canPlaceNumber, index + 1);
+                    canPlaceNumber[i] = false;
+                }
+            }
+        }
+
+        public static bool IsValidNumber(int i, int current, int previous)
+        {
+            if (i == 0)
+            {
+                return true;
+            }
+            else if (i % 2 == 0)
+            {
+                if (current > previous)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (current < previous)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
