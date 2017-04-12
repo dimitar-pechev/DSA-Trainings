caa7397d8f215d3fe7b4ced33f5893ef822c5b50
dimitar-pechev <dimpechev@gmail.com>
2017-04-12 12:24:54 +0300

    more exercises

---
+++ WorkshopsAndExamTasks/Passwords/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Passwords
+{
+    public class Startup
+    {
+        private static int counter;
+        private static bool isDone;
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var pattern = Console.ReadLine();
+            var k = int.Parse(Console.ReadLine());
+
+            var password = new int[n];
+
+            GetKthPassword(password, pattern, n, k);
+        }
+
+        private static void GetKthPassword(int[] password, string pattern, int n, int k, int index = 0)
+        {
+            if (isDone)
+            {
+                return;
+            }
+            
+            if (index == n)
+            {
+                counter++;
+
+                if (k == counter)
+                {
+                    Console.WriteLine(string.Join("", password));
+                    isDone = true;
+                }
+
+                return;
+            }
+
+            if (index == 0)
+            {
+                password[index] = 0;
+                GetKthPassword(password, pattern, n, k, index + 1);
+
+                for (int i = 1; i <= 9; i++)
+                {
+                    password[index] = i;
+                    GetKthPassword(password, pattern, n, k, index + 1);
+                }                
+            }
+            else
+            {
+                if (pattern[index - 1] == '=')
+                {
+                    password[index] = password[index - 1];
+                    GetKthPassword(password, pattern, n, k, index + 1);
+                }
+                else if (pattern[index - 1] == '>')
+                {
+                    if (password[index - 1] == 0)
+                    {
+                        return;
+                    }
+                    
+                    password[index] = 0;
+                    GetKthPassword(password, pattern, n, k, index + 1);
+
+                    for (int i = password[index - 1] + 1; i <= 9; i++)
+                    {
+                        password[index] = i;
+                        GetKthPassword(password, pattern, n, k, index + 1);
+                    }
+                }
+                else if (pattern[index - 1] == '<')
+                {
+                    if (password[index - 1] == 1)
+                    {
+                        return;
+                    }
+
+                    var end = password[index - 1] != 0 ? password[index - 1] - 1 : 9;
+
+                    for (int i = 1; i <= end; i++)
+                    {
+                        password[index] = i;
+                        GetKthPassword(password, pattern, n, k, index + 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WorkshopsAndExamTasks/ReversePolishNotation/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReversePolishNotation
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var expression = Console.ReadLine().Split(' ').ToArray();
+
+            var stack = new Stack<int>();
+            
+            foreach (var element in expression)
+            {
+                int number;
+                if (int.TryParse(element, out number))
+                {
+                    stack.Push(number);
+                    continue;
+                }
+
+                var secondNumber = stack.Pop();
+                var firstNumber = stack.Pop();
+
+                stack.Push(CalcultateExpression(firstNumber, secondNumber, element));
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+
+        private static int CalcultateExpression(int firstNumber, int secondNumber, string item)
+        {
+            switch (item)
+            {
+                case "+": return firstNumber + secondNumber;
+                case "-": return firstNumber - secondNumber;
+                case "*": return firstNumber * secondNumber;
+                case "/": return firstNumber / secondNumber;
+                case "&": return firstNumber & secondNumber;
+                case "^": return firstNumber ^ secondNumber;
+                case "|": return firstNumber | secondNumber;
+                default: throw new InvalidOperationException();
+            }
+        }
+    }
+}
